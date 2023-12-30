66dec012585aac9455163c3f802e46b57dbdb18a
Satyam Gupta <sg4684180@gmail.com>
2023-12-31 00:07:29 +0530

    adopt page updated

--- script/adopt.js
+++ script/adopt.js
@@ -1,36 +1,59 @@
-// init Isotope
-var $grid = $('#product-list').isotope({
-    // options
-  });
-  // filter items on button click
-  $('.filter-button-group').on( 'click', 'button', function() {
-    var filterValue = $(this).attr('data-filter');
-    $grid.isotope({ filter: filterValue });
-  });
-function fun(){
-    window.open("search.html","_self");
+function searchPet() {
+    var searchVal = document.getElementById('search').value;
+    console.log(searchVal);
+    if(searchVal === 'dog') {
+        func(1);
+    }
+    else if(searchVal === 'cat') {
+        func(2);
+    }
+    else if(searchVal === 'rabbit') {
+        func(3);
+    }
 }
-$(document).ready(function(){
-    $('search').click(function(){
-        alert("search");
-        var checker=0;
-        for(var i=0;i<12;i++){
-            var card=$("div").filter(".col-lg-4")[i];
-            var title=$("h5").filter(".d-block")[i].innerText.toUpperCase();
 
-            if(title.indexOf($("#inp").val().toUpperCase())>-1){
-                card.style.display='block';
-                checker++;
-            } 
-            else{
-                card.style.display='none';
-            }
-        }
-        if(checker>0){
-            $("#not_find_any_thing").text("");
-        }
-        else{
-            $("#not_find_any_thing").text("No Result Found").fadeIn();
-        }
-    })
-  });
+
+
+function func(type) {
+    console.log(type);
+    var id = document.getElementById(type);
+    var row = document.querySelector("#row");
+    console.log(row);
+    document.getElementsByClassName('active')[0].classList.remove('active');
+    if(type == 0) {
+        var collection = row.querySelectorAll(".hidden");
+        removeClass(collection);
+        id.classList.add("active");
+        return;
+    }
+    // console.log(collection);
+    var dogs = row.querySelectorAll(".dog");
+    var cats = row.querySelectorAll(".cat");
+    var rabbits = row.querySelectorAll(".rabbit");
+    if(type == 1) {
+        addClass(cats);
+        addClass(rabbits);
+        removeClass(dogs);
+    }
+    else if(type == 2) {
+        addClass(dogs);
+        addClass(rabbits);
+        removeClass(cats)
+    }
+    else {
+        addClass(dogs);
+        addClass(cats);
+        removeClass(rabbits)
+    }
+    id.classList.add("active");
+}
+function addClass(collection) {
+    for (var i=0; i<collection.length; i++) {
+        collection[i].classList.add('hidden');
+    }
+}
+function removeClass(collection) {
+    for (var i=0; i<collection.length; i++) {
+        collection[i].classList.remove('hidden');
+    }
+}
